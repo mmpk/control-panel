2ca98836d5fa2e2913d3b55cb5a118c8ee26b4fe
mmpk <noreply>
2014-09-22 19:03:54 +0000

    Add component nav-left

---
+++ ui/nav-left.reel/nav-left.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/nav-left.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class NavLeft
+ * @extends Component
+ */
+exports.NavLeft = Component.specialize(/** @lends NavLeft# */ {
+    constructor: {
+        value: function NavLeft() {
+            this.super();
+        }
+    }
+});
