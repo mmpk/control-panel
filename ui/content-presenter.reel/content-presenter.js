94bde3a299888ed12cb873d96ae937e2c9504efc
mmpk <noreply>
2014-09-22 23:19:03 +0000

    Add component content-presenter

---
+++ ui/content-presenter.reel/content-presenter.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/content-presenter.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class ContentPresenter
+ * @extends Component
+ */
+exports.ContentPresenter = Component.specialize(/** @lends ContentPresenter# */ {
+    constructor: {
+        value: function ContentPresenter() {
+            this.super();
+        }
+    }
+});
