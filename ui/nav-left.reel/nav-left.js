33dc4a388d6f6d1fb2cb83b0e16b8e7ac4893537
mmpk <noreply>
2014-09-23 03:07:04 +0000

    Update component ui/nav-left.reel/nav-left.js

--- ui/nav-left.reel/nav-left.js
+++ ui/nav-left.reel/nav-left.js
@@ -26,7 +26,7 @@ exports.NavLeft = Component.specialize(/** @lends NavLeft# */ {
         ]
     },
     
-    handleHoverAction: {
+    handleButtonAction: {
         value: function(v) {
             var rep =  this.templateObjects.repetition;
              console.log("mouseover")
