dfc70e9bc43d8d52c3ef57d3d6ebcff0f7cdc228
mmpk <noreply>
2014-09-23 23:36:33 +0000

    Update component ui/toggle-btn.reel/toggle-btn.js

--- ui/toggle-btn.reel/toggle-btn.js
+++ ui/toggle-btn.reel/toggle-btn.js
@@ -23,7 +23,6 @@ exports.ToggleBtn = AbstractCheckbox.specialize(/** @lends ToggleBtn# */ {
             return this._value;
         },
         set: function(value) {
-              console.log(value)
             if (this._value !== value) {
                 this._value = value;
                 this.needsDraw = true;
