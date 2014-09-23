8d3a0812179ef8a316be39a3924673ed645fb2e3
mmpk <noreply>
2014-09-23 05:13:58 +0000

    Update component ui/nav-left.reel/nav-left.js

--- ui/nav-left.reel/nav-left.js
+++ ui/nav-left.reel/nav-left.js
@@ -29,7 +29,7 @@ exports.NavLeft = Component.specialize(/** @lends NavLeft# */ {
     handleButtonAction: {
         value: function(v) {
             var p =  this;
-             console.log(p.className)
+             console.log(p.classList)
         }
     }
 });
