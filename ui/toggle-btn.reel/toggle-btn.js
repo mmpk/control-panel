7b6935f3b3dad4a8aacb66c4d6e21e484f308464
mmpk <noreply>
2014-09-23 17:32:22 +0000

    Add component toggle-btn

---
+++ ui/toggle-btn.reel/toggle-btn.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/toggle-btn.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class ToggleBtn
+ * @extends Component
+ */
+exports.ToggleBtn = Component.specialize(/** @lends ToggleBtn# */ {
+    constructor: {
+        value: function ToggleBtn() {
+            this.super();
+        }
+    }
+});
