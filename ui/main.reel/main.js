171430252572848cd940283bf97ba9802520a910
mmpk <noreply>
2014-09-23 23:55:32 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -27,7 +27,7 @@ exports.Main = Component.specialize(/** @lends Main# */ {
     templateDidLoad: {
         value: function(){
             var exp = this.templateObjects.expander;
-            exp.element.appendChild('<div style="width: 180px;background:pink;">PINKY</div>');
+            exp.element.appendChild('APPENDAAAAAAA');
             console.log(exp.element)
             
              var subt = this.templateObjects.substitution;
