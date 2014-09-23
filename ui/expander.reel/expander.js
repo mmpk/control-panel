a2e0676c157af3a0b61ec57d4f4f26939cd57d44
mmpk <noreply>
2014-09-23 17:29:55 +0000

    Add component expander

---
+++ ui/expander.reel/expander.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/expander.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Expander
+ * @extends Component
+ */
+exports.Expander = Component.specialize(/** @lends Expander# */ {
+    constructor: {
+        value: function Expander() {
+            this.super();
+        }
+    }
+});
