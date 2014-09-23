b52095b59adc9f9bf82160d000583c8923bae9c6
mmpk <noreply>
2014-09-23 01:38:32 +0000

    Add component table-view

---
+++ ui/table-view.reel/table-view.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/table-view.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TableView
+ * @extends Component
+ */
+exports.TableView = Component.specialize(/** @lends TableView# */ {
+    constructor: {
+        value: function TableView() {
+            this.super();
+        }
+    }
+});
