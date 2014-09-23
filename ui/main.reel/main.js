31371d76c2b87a44e49e72c9c497519385104554
mmpk <noreply>
2014-09-23 23:42:14 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -28,7 +28,7 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         value: function(){
             var exp = this.templateObjects.expander;
             exp.element.innerHTML = '<div style="width: 180px;background:pink;">PINKY</div>';
-            console.log(exp.innerHTML)
+            console.log(exp.lement.children)
             
              var subt = this.templateObjects.substitution;
              var img = this.templateObjects.image2;
