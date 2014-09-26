e6b7707deafb3e86022161ad7a4542378c626750
mmpk <noreply>
2014-09-26 10:13:38 +0000

    Add component calendar

---
+++ ui/calendar.reel/calendar.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/calendar.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Calendar
+ * @extends Component
+ */
+exports.Calendar = Component.specialize(/** @lends Calendar# */ {
+    constructor: {
+        value: function Calendar() {
+            this.super();
+        }
+    }
+});
