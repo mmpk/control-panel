31ba42d5630cf2803e21c4c79c89ef39343ceb00
mmpk <noreply>
2014-09-26 10:29:31 +0000

    Update component ui/calendar.reel/calendar.js

--- ui/calendar.reel/calendar.js
+++ ui/calendar.reel/calendar.js
@@ -4,6 +4,12 @@
  */
 var Component = require("montage/ui/component").Component;
 
+var cal_days_labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    cal_months_labels = ['January', 'February', 'March', 'April',
+                         'May', 'June', 'July', 'August', 'September',
+                         'October', 'November', 'December'],
+    cal_days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+    cal_current_date = new Date(); 
 /**
  * @class Calendar
  * @extends Component
@@ -13,5 +19,21 @@ exports.Calendar = Component.specialize(/** @lends Calendar# */ {
         value: function Calendar() {
             this.super();
         }
-    }
+    },
+    templateDidLoad: {
+        value: function() {
+            this.Calendar(cal_current_date.getMonth(), cal_current_date.getFullYear())
+            
+        }
+    },
+    constructor: {
+        value: function Calendar(month, year) {
+             console.log("Calendar:")
+            this.month = (isNaN(month) || month == null) ? cal_current_date.getMonth() : month;
+            this.year  = (isNaN(year) || year == null) ? cal_current_date.getFullYear() : year;
+            this.html = '';
+             console.log(this.month)
+             console.log(this.year)
+        }
+    },
 });
