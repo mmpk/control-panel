bc561d791d27afcfe80e90279b316f308583950e
mmpk <noreply>
2014-09-26 19:16:51 +0000

    Update component ui/calendar.reel/calendar.js

--- ui/calendar.reel/calendar.js
+++ ui/calendar.reel/calendar.js
@@ -24,7 +24,7 @@ exports.Calendar = Component.specialize(/** @lends Calendar# */ {
         value: function() {
             var mc = this.templateObjects.monthController
             mc.select(mc.content[cal_current_date.getMonth()])
-            mc.observePath("selection.0", this.onChangeMonth(this)) 
+            mc.observePath("selection.0", this.onChangeMonth("say")) 
         }
     },
     data: {
