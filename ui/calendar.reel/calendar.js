7eb86827d045bae66b96557951ca85288d26f9af
mmpk <noreply>
2014-09-27 13:02:42 +0000

    Update component ui/calendar.reel/calendar.js

--- ui/calendar.reel/calendar.js
+++ ui/calendar.reel/calendar.js
@@ -23,7 +23,7 @@ exports.Calendar = Component.specialize(/** @lends Calendar# */ {
     enterDocument: {
         value: function(firstTime) {
              console.log("ENTER")
-            console.log(this.templateObjects = documentPart.objects;)
+            console.log(this.templateObjects = documentPart.objects)
         }
     },
     templateDidLoad: {
