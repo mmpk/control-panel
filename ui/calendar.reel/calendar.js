7670a3ea569571a042854efe5e2c0cd8dca27dfe
mmpk <noreply>
2014-09-28 11:42:59 +0000

    Update component ui/calendar.reel/calendar.js

--- ui/calendar.reel/calendar.js
+++ ui/calendar.reel/calendar.js
@@ -40,7 +40,7 @@ exports.Calendar = Component.specialize(/** @lends Calendar# */ {
     maxMonth: {
         value: function(v) {
             console.log("minMonth")
-            var mc = this.templateObjects.monthController
+            var mc = v.templateObjects.monthController
             var idx = mc.content.indexOf(mc.selection[0])
             mc.select(mc.content[idx+1])
             console.log(mc.selection[0].month)
