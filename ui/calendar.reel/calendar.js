fc9f13cd7717cd51ca75f2ca723420502589645e
mmpk <noreply>
2014-09-29 19:46:13 +0000

    Update component ui/calendar.reel/calendar.js

--- ui/calendar.reel/calendar.js
+++ ui/calendar.reel/calendar.js
@@ -64,7 +64,7 @@ exports.Calendar = Component.specialize(/** @lends Calendar# */ {
         value: function(){
             var rep = this.templateObjects.repetition;
             var month = this.date.getMonth()
-            var year  = this.getFullYear()
+            var year  = this.date.getFullYear()
             var days = new Date(year, 1, 0).getDate()
             var arr = []
             for(var i=0;i<days;i++){
