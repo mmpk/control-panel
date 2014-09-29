fcb2ffcdfa1381f3a4e7ac26864af36a6451faa5
mmpk <noreply>
2014-09-29 22:17:56 +0000

    Update component ui/calendar.reel/calendar.js

--- ui/calendar.reel/calendar.js
+++ ui/calendar.reel/calendar.js
@@ -55,7 +55,7 @@ exports.Calendar = Component.specialize(/** @lends Calendar# */ {
 
     },
     fillDays: {
-        value: function(){
+        value: function(v){
             //var mc = this.templateObjects.monthController;          
             var rep = this.templateObjects.repetition;
             var month = 3
