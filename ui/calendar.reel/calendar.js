6bee392fcaa02893214e78ed36d0f2aa7fd2a0bd
mmpk <noreply>
2014-09-29 22:20:35 +0000

    Update component ui/calendar.reel/calendar.js

--- ui/calendar.reel/calendar.js
+++ ui/calendar.reel/calendar.js
@@ -69,6 +69,7 @@ exports.Calendar = Component.specialize(/** @lends Calendar# */ {
                 arr.push(i)
             }
             rep.content = arr
+            console.log(days);
         }
     },
     data: {
