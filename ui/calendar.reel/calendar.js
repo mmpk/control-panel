0588959fe3c2ae96e22e36580c526e13ed6f20bf
mmpk <noreply>
2014-09-29 20:59:27 +0000

    Update component ui/calendar.reel/calendar.js

--- ui/calendar.reel/calendar.js
+++ ui/calendar.reel/calendar.js
@@ -28,7 +28,9 @@ exports.Calendar = Component.specialize(/** @lends Calendar# */ {
              var mc = this.templateObjects.monthController
              mc.select(mc.content[this.date.getMonth()])
              
-             var a = document.querySelectorAll("#month_minus,#month_plus,#year_plus,#year_minus").addEventListener("mouseup", this, false);
+             var a = document.querySelectorAll("#month_minus,#month_plus,#year_plus,#year_minus").forEach(function(v){
+                 v.addEventListener("mouseup", this, false);
+             })
              console.log("LOCATION:");
              console.log(window.location.host);   
              var b = document.querySelector(".iwraper").addEventListener("mousedown", this, false);
