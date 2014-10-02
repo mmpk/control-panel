da11d5cc803be71faf42085dd4c70a13ebecce07
mmpk <noreply>
2014-10-02 21:27:26 +0000

    Add module tab

---
+++ ui/tab-control.reel/tab.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/tab-control.reel/tab
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Tab
+ * @extends Montage
+ */
+exports.Tab = Montage.specialize(/** @lends Tab# */ {
+    constructor: {
+        value: function Tab() {
+            this.super();
+        }
+    }
+});
