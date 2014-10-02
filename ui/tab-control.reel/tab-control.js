3affff3cce41cd3c6b99f1a0197abd79e0846069
mmpk <noreply>
2014-10-02 21:22:02 +0000

    Add component tab-control

---
+++ ui/tab-control.reel/tab-control.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/tab-control.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TabControl
+ * @extends Component
+ */
+exports.TabControl = Component.specialize(/** @lends TabControl# */ {
+    constructor: {
+        value: function TabControl() {
+            this.super();
+        }
+    }
+});
