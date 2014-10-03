33f847d8196d7a12f197a93c06ed28d6164349b7
mmpk <noreply>
2014-10-03 12:44:42 +0000

    Update component ui/tab-control.reel/tab.reel/tab.js

--- ui/tab-control.reel/tab.reel/tab.js
+++ ui/tab-control.reel/tab.reel/tab.js
@@ -17,7 +17,7 @@ exports.Tab = Component.specialize(/** @lends Tab# */ {
     enterDocument: {
         value: function(firstTime) {
             var glyph = document.querySelector(".glyphicon")
-                glyph.classList.addClass('glyphicon-'+ this._glyph)
+                //glyph.classList.addClass('glyphicon-'+ this._glyph)
                         console.log("glyph")
                         console.log(glyph)
             
