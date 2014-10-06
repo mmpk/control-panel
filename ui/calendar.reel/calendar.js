0578bf0860c617f68a63d02b7a44efd13a1f0c02
mmpk <noreply>
2014-10-06 00:09:53 +0000

    Update component ui/calendar.reel/calendar.js

--- ui/calendar.reel/calendar.js
+++ ui/calendar.reel/calendar.js
@@ -40,7 +40,7 @@ exports.Calendar = Component.specialize(/** @lends Calendar# */ {
  */
              var b = document.querySelector(".iwraper").addEventListener("mousedown", this, false);
              var mc = this.templateObjects.monthController;   
-                mc.addBeforePathChangeListener("selection", this, handleBeforeChange)
+                mc.addBeforePathChangeListener("selection", this.handleBeforeChange)
              var yInput = this.templateObjects.yearInput
                  yInput.value = this.date.getFullYear()
 
